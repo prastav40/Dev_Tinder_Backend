379ffa942ddadbab35ac808451eb5422647e7020
Prastav Deokar <deokarprastav40@gmail.com>
2026-09-10 21:28:50 +0530

    added coonections,acceptednotaccepted pendingrequest routes

--- src/routes/request.ts
+++ src/routes/request.ts
@@ -33,4 +33,85 @@ RequestRouterRequest.post("/request/send/:status/:touserid", userAuth, async (re
     }
 });
 
+
+RequestRouterRequest.get("/user/requests/pendingrequests", userAuth, async (req: Request, res: Response) => {
+  try {
+    const loggedInUser = req.user;
+
+    const pendingRequests = await ConnectionRequest.find({
+      toUserId: loggedInUser._id,
+      status: "interested",
+    }).populate(
+      "fromUserId", 
+      "firstName lastName skills age about"
+    );
+
+    res.status(200).json({
+      message: "Data fetched successfully",
+      data: pendingRequests,
+    });
+
+  } catch (err: any) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+RequestRouterRequest.post("/user/requests/:accept_notaccept/:personuserid", userAuth, async (req: Request, res: Response) => {
+    const decideresult = req.params.accept_notaccept as string;
+    const touserid = req.params.personuserid as string;
+    const loggedInUser = req.user._id;
+
+    const allowedDecisions = ["accepted", "rejected"];
+
+    if (!allowedDecisions.includes(decideresult)) {
+        return res.status(400).send("Invalid decision value. Allowed values are 'accepted' or 'rejected'.");
+    }
+
+    try {
+        const selectedresult = await ConnectionRequest.findOne({
+            fromUserId: touserid,
+            toUserId: loggedInUser,
+            status: "interested"
+        });
+
+        if (!selectedresult) {
+            return res.status(404).send("No pending request found from this user.");
+        }
+            
+        selectedresult.status = decideresult as "accepted" | "rejected";
+        await selectedresult.save();
+        
+        res.status(200).send("Request processed successfully.");
+    } catch (error) {
+        res.status(500).send("An error occurred while processing the request.");
+    }
+});
+
+RequestRouterRequest.get("/user/connections", userAuth, async (req: Request, res: Response) => {
+  try {
+    const loggedInUser = req.user;
+
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [
+        { fromUserId: loggedInUser._id, status: "accepted" },
+        { toUserId: loggedInUser._id, status: "accepted" },
+      ],
+    })
+      .populate("fromUserId", "firstName lastName skills age about")
+      .populate("toUserId", "firstName lastName skills age about");
+
+    const data = connectionRequests.map((row) => {
+      if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+        return row.toUserId; 
+      }
+      return row.fromUserId;  
+    });
+
+    res.status(200).json({ data });
+
+  } catch (err: any) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
 export { RequestRouterRequest }; 
